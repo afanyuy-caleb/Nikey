908234c25c2729f598185847b6e7aa44145c1f43
keyz-dev <calkeyz0313@gmail.com>
2025-03-03 12:43:55 +0100

    configured the vite

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    strictPort: true,
-    hmr: {
-        host: '192.168.43.32',
-        port: 5173,
-    },
-  }
+  // server: {
+  //   host: '0.0.0.0',
+  //   port: 5173,
+  //   strictPort: true,
+  //   hmr: {
+  //       host: '192.168.43.32',
+  //       port: 5173,
+  //   },
+  // }
 })
